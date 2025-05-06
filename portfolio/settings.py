bae8b2d84cb4e1f212f61961d8386fb0277acdb4
Patrick Odeh <vyltyan@gmail.com>
2025-05-06 02:45:07 +0100

    collected static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
